71b6532abd60909df09d52d04b58870904f075d5
sbg15236 <35972385+sbg15236@users.noreply.github.com>
2018-04-12 13:57:25 -0400

    SBG Updates
    Just a few changes to the code to check the header data instead of printing the whole dataset for our sanity checks. Otherwise, we need to test the code. Note: the code won't run unless you have file paths specified for the CSV files you want to open so whoever is going to run this for the presentation needs to make sure the filepath is correct or our code is broken from the beginning.

--- Prototype Cancer Cell Lines.py
+++ Prototype Cancer Cell Lines.py
@@ -4,50 +4,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 apopcand = pd.read_csv('Apo_Mutations.tar.gz', error_bad_lines=False)
-print(apopcand)
+print(apopcand(head())
 apopcand.to_csv('apopcand.csv')
 apopcand['mutation_ccle_cosmic'] = apopcand['mutation_ccle_cosmic'].apply(lambda x:x.split('')[0])
 
-#reading the csv files with pandas
+#reading the csv files with pandas and checking data
 achilles=pd.read_csv('Achilles_v2.4_SampleInfo_small.csv')
-#print(achilles)
+print(achilles(head())
 
 gene_dependency=pd.read_csv('gene_dependency.csv')
-#print(gene_dependency)
+print(gene_dependency(head())
 
 #selecting only KRas mutant cell lines from the csv
 remove_WT=achilles.loc[(achilles['KRAS'] != 'WT') & (achilles['KRAS'] !='NA/WT')]
 solamente=remove_WT.dropna(subset=['KRAS'])
 
 #truncate file to only cell lines and tumor type
-truncate=solamente.truncate(before='Name', after='Type', axis=1)
-truncate.to_csv('truncate.csv')
+trunc=solamente.truncate(before='Name', after='Type', axis=1)
+trunc.to_csv('truncate.csv')
 
 #merge Dataframes based on KRas mutants
-#match_column=truncate.merge(truncate, gene_dependency, how='left', left_on=None, right_on=None)
+match_column=truncate.merge(trunc, gene_dependency, how='left', left_on=None, right_on=None)
 match_column=truncate.merge(gene_dependency, how='inner', left_on='Name', right_on='line')
 
-print (match_column)
+print (match_column(head())
 
 #generate csv to examine output
 merged3=match_column.to_csv('merged.csv')
 
 #removing unessessary columns
-#unify=match_column.loc[:, 'line':]
-#unify = unify.rename(columns={'line': 'Name'})
+unify=match_column.loc[:, 'line':]
+unify = unify.rename(columns={'line': 'Name'})
 
 #Renaming the cell lines in the 'Name' columns
-#unify['Name'] = unify['Name'].apply(lambda x: x.split('_')[0])
-#unify.to_csv('unify.csv')
-
+unify['Name'] = unify['Name'].apply(lambda x: x.split('_')[0])
+unify.to_csv('unify.csv')
+print(unify(head())
 
 #Set index to cell line names
 #create dictionary after slightly changing dataframe structure
-#kras_dictionary=unify.set_index('Name').T.to_dict('list')
-#print(type(kras_dictionary))
+kras_dictionary=unify.set_index('Name').T.to_dict('list')
+print(type(kras_dictionary))
 
 #Test run for a specific cell line
-#print(kras_dictionary['LOVO'])
+print(kras_dictionary['LOVO'])
 
 
 
@@ -55,9 +55,9 @@ merged3=match_column.to_csv('merged.csv')
 
 #Create a list from the dataframe column names and delete the first member 'Name'
 #columns_names=list(unify.columns.values)
-#print(columns_names)
-#del columns_names[0]
-#print(columns_names)
+print(unify.columns_names)
+del columns_names[0]
+print(unify.columns_names)
 
-#plt.hist(list(kras_dictionary.keys()), kras_dictionary.values(), color='g')
-#plt.show()
+plt.hist(list(kras_dictionary.keys()), kras_dictionary.values(), color='g')
+plt.show()
