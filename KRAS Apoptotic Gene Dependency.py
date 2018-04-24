bdda3d03162779a11a10ccb478b16783777688fc
onconomics <38267613+onconomics@users.noreply.github.com>
2018-04-24 17:10:33 -0400

    Update KRAS Apoptotic Gene Dependency.py

--- KRAS Apoptotic Gene Dependency.py
+++ KRAS Apoptotic Gene Dependency.py
@@ -15,3 +15,19 @@ import seaborn as sns
 achilles=pd.read_csv('Achilles_v2.4_SampleInfo_small.csv')
 achilles.head()
 achilles.shape
+
+#CRISPR gene dependency data sourced from Project Achilles avana_public_18Q1, v3 data folder
+#(https://portals.broadinstitute.org/achilles/datasets/18/download)
+gene_dependency=pd.read_csv('gene_dependency.csv')
+gene_dependency.head()
+gene_dependency.shape
+
+#selecting only KRas mutant cell lines from csv
+remove_WT=achilles.loc[(achilles['KRAS'] != 'WT') & (achilles['KRAS'] !='NA/WT')]
+solamente=remove_WT.dropna(subset=['KRAS'])
+solamente.head()
+
+#truncate file to only cell lines and tumor type
+trunc=solamente.truncate(before='Name', after='Type', axis=1)
+trunc.shape
+trunc.head()
